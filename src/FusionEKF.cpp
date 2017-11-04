95cd4220a8d0743a67542e906a5f714a674baf66
Marc Engeler <marc.engeler@gmail.com>
2017-11-04 02:36:12 +0100

    Bugfree Commit

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -139,9 +139,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 	ekf_.R_ = R_radar_;
 	ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
-    /*ekf_.H_ = H_laser_;
+    ekf_.H_ = H_laser_;
 	ekf_.R_ = R_laser_;
-	ekf_.Update(measurement_pack.raw_measurements_);*/
+	ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
